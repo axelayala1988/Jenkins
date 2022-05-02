51baa097e64b33e117a06a2aebbc6d3c127f0533
axelayala1988 <56412981+axelayala1988@users.noreply.github.com>
2022-05-02 13:45:25 -0500

    Added a new entry in the test
    For the sign of the pentragram

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,4 +44,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingPlease() {
+    String someone = "for with the sign of the pentagram";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
