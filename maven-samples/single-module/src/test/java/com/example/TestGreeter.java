c0dbf3e553f786ac1a5877345b94e36d0eced575
axelayala1988 <56412981+axelayala1988@users.noreply.github.com>
2022-05-02 13:54:38 -0500

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -44,11 +44,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-  
-  @Test
-  public void greetShouldIncludeGreetingPlease() {
-    String someone = "for with the sign of the pentagram";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
